0d8858e4943e7debc05d9ad401c95d77e6633e39
kobir hossain Ritu <kobir.h.ritu@gmail.com>
2022-07-14 22:14:49 +0600

    Food Item Changed

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -12,8 +12,8 @@ const DUMMY_MEALS = [
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
+    name: 'Butter Chicken',
+    description: 'An Indian Dish',
     price: 16.5,
   },
   {
@@ -24,7 +24,7 @@ const DUMMY_MEALS = [
   },
   {
     id: 'm4',
-    name: 'Green Bowl',
+    name: 'Green Salad',
     description: 'Healthy...and green...',
     price: 18.99,
   },
